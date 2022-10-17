3127fe103bf5e5044b3c8b1eec2f104f1c3aee39
Anna An <arzumanyan.an@gmail.com>
2022-10-17 10:00:21 +0400

    Chapter 9 files

--- 09TUT/controllers/employeesController.js
+++ 09TUT/controllers/employeesController.js
@@ -5,13 +5,15 @@ const data = {
   },
 };
 
-const getAllEployees = (req, res) => {
-  res.join(data.employees);
+const getAllEmployees = (req, res) => {
+  res.json(data.employees);
 };
 
 const createNewEmployee = (req, res) => {
   const newEmployee = {
-    id: data.employees[data.employees.length - 1].id + 1 || 1,
+    id: data.employees?.length
+      ? data.employees[data.employees.length - 1].id + 1
+      : 1,
     firstname: req.body.firstname,
     lastname: req.body.lastname,
   };
@@ -27,22 +29,56 @@ const createNewEmployee = (req, res) => {
 };
 
 const updateEmployee = (req, res) => {
-  res.json({
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-  });
+  const employee = data.employees.find(
+    (emp) => emp.id === parseInt(req.body.id)
+  );
+  if (!employee) {
+    return res
+      .status(400)
+      .json({ message: `Employee ID ${req.body.id} not found` });
+  }
+  if (req.body.firstname) employee.firstname = req.body.firstname;
+  if (req.body.lastname) employee.lastname = req.body.lastname;
+  const filteredArray = data.employees.filter(
+    (emp) => emp.id !== parseInt(req.body.id)
+  );
+  const unsortedArray = [...filteredArray, employee];
+  data.setEmployees(
+    unsortedArray.sort((a, b) => (a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
+  );
+  res.json(data.employees);
 };
 
 const deleteEmployee = (req, res) => {
-  res.json({ id: req.body.id });
+  const employee = data.employees.find(
+    (emp) => emp.id === parseInt(req.body.id)
+  );
+  if (!employee) {
+    return res
+      .status(400)
+      .json({ message: `Employee ID ${req.body.id} not found` });
+  }
+  const filteredArray = data.employees.filter(
+    (emp) => emp.id !== parseInt(req.body.id)
+  );
+  data.setEmployees([...filteredArray]);
+  res.json(data.employees);
 };
 
 const getEmployee = (req, res) => {
-  res.json({ id: req.params.id });
+  const employee = data.employees.find(
+    (emp) => emp.id === parseInt(req.params.id)
+  );
+  if (!employee) {
+    return res
+      .status(400)
+      .json({ message: `Employee ID ${req.params.id} not found` });
+  }
+  res.json(employee);
 };
 
 module.exports = {
-  getAllEployees,
+  getAllEmployees,
   createNewEmployee,
   updateEmployee,
   deleteEmployee,
--- 09TUT/routes/api/employees.js
+++ 09TUT/routes/api/employees.js
@@ -1,15 +1,30 @@
+// const express = require("express");
+// const router = express.Router();
+// const employeesController = require("../../controllers/employeesController");
+
+// router
+//   .route("/")
+//   .get(employeesController.getAllEployees)
+//   .post(employeesController.createNewEmployee)
+//   .put(employeesController.updateEmployee)
+//   .delete(employeesController.deleteEmployee);
+
+// //   showing the parameter directly in the url
+// router.route("/:id").get(employeesController.getEmployee);
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 const employeesController = require("../../controllers/employeesController");
 
 router
   .route("/")
-  .get(employeesController.getAllEployees)
+  .get(employeesController.getAllEmployees)
   .post(employeesController.createNewEmployee)
   .put(employeesController.updateEmployee)
   .delete(employeesController.deleteEmployee);
 
-//   showing the parameter directly in the url
 router.route("/:id").get(employeesController.getEmployee);
 
 module.exports = router;
