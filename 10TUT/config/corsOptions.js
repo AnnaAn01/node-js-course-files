1893b6d2ef7a7bf4ed9fb16ac44ca84b4b16b7da
Anna An <arzumanyan.an@gmail.com>
2022-10-18 15:29:02 +0400

    adding Chapter 10 starter files

---
+++ 10TUT/config/corsOptions.js
@@ -0,0 +1,20 @@
+const whiteList = [
+  "https://www.yoursite.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    // if the domain is in the witelist, then we'll let it pass
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      // null is usually the error
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
